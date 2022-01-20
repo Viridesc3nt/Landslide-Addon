6acb8b62e4b0fb83de154d2f87993ada5661ae83
Viridesc3nt <97551484+Viridesc3nt@users.noreply.github.com>
2022-01-20 17:37:54 -0500

    Update LandslideListener.java

--- LandslideListener.java
+++ LandslideListener.java
@@ -3,6 +3,7 @@ package me.justinjaques.landslide;
 import com.projectkorra.projectkorra.BendingPlayer;
 import com.projectkorra.projectkorra.ProjectKorra;
 import com.projectkorra.projectkorra.ability.CoreAbility;
+import com.projectkorra.projectkorra.util.TempBlock;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -10,12 +11,20 @@ import org.bukkit.event.block.Action;
 import org.bukkit.event.entity.EntityChangeBlockEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerToggleSneakEvent;
+import org.bukkit.metadata.FixedMetadataValue;
+
+import java.util.Objects;
+
 
 public class LandslideListener implements Listener {
     @EventHandler
     public void onSneak(PlayerToggleSneakEvent event) {
         Player player = event.getPlayer();
         BendingPlayer bPlayer = BendingPlayer.getBendingPlayer(player);
+
+        if(CoreAbility.getAbility(Landslide.class).isStarted()) {
+            event.setCancelled(true);
+        }
         if(bPlayer.canBend(CoreAbility.getAbility(Landslide.class))) {
             new Landslide(player);
         }
@@ -24,7 +33,8 @@ public class LandslideListener implements Listener {
     @EventHandler
     public void check(EntityChangeBlockEvent event) {
         if (event.getEntity().hasMetadata("Landslide")) {
-            //event.setCancelled(true);
+            ((TempBlock) Objects.requireNonNull(event.getEntity().getMetadata("PK::Viridescent::Landslide").get(0).value())).revertBlock();
+            event.setCancelled(true);
         }
 
     }
